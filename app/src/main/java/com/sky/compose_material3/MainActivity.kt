2108dff4767aa6bfe971a3442b18f5ad6c738ed5
Mahmoud Mirghafari <mahmoud.mirghafari@gmail.com>
2022-06-01 10:57:19 +0430

    the first version of material 3 with compose

---
+++ app/src/main/java/com/sky/compose_material3/MainActivity.kt
@@ -0,0 +1,191 @@
+package com.sky.compose_material3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.sky.compose_material3.ui.theme.ComposeMaterial3Theme
+import kotlin.math.round
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainPage()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPage() {
+    ComposeMaterial3Theme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+
+            Scaffold(
+                topBar = {
+                    SmallTopAppBar(
+                        title = {
+                            Text(text = "Compose material3 sample!")
+                        },
+                        colors = TopAppBarDefaults.smallTopAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                },
+            ) { paddingValues ->
+
+                LazyColumn(contentPadding = paddingValues) {
+                    items(30) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            CardItem(
+                                title = "Some title",
+                                description = "Some descriptions to fill this part of view. some descriptions to fill this part of view. some descriptions to fill this part of view.",
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardItem(
+    title: String,
+    description: String,
+    modifier: Modifier
+) {
+
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.large,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter("https://picsum.photos/seed/${Random.nextInt()}/300/200"),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(3f / 2)
+                .clip(MaterialTheme.shapes.large),
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+
+            Text(text = title, style = MaterialTheme.typography.headlineMedium)
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(text = description, style = MaterialTheme.typography.bodyMedium)
+
+            Spacer(Modifier.height(12.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .weight(1f)
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .clickable { }
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
+                    Text(
+                        text = "Make as favorite",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Row(
+                    modifier = Modifier
+                        .weight(1f)
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .clickable { }
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(imageVector = Icons.Default.Share, contentDescription = null)
+                    Text(
+                        text = "Share with others",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+fun getContentData() = listOf(
+    "sssss",
+    "sssss",
+    "sssss",
+    "sssss",
+    "sssss"
+)
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainPage()
+}
